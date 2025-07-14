c4bc25b857373531c2bfbcec7483f8575e4a0f35
Gattskii <117891948+Gattskii@users.noreply.github.com>
2025-07-14 21:35:55 +0700

    File upload

---
+++ Db.cs
@@ -0,0 +1,51 @@
+namespace PizzaStore.DB;
+
+public record Pizza
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
+
+public class PizzaDB
+{
+    private static List<Pizza> _pizzas = new List<Pizza>()
+    {
+        new Pizza{Id = 1, Name="Pizzahut, Yummy af we love it"},
+        new Pizza{Id = 2, Name="Domino, Tasty but too thin"},
+        new Pizza{Id = 3, Name="Marzano, Classic and nostalgic"}
+    };
+
+    public static List<Pizza> GetPizzas()
+    {
+        return _pizzas;
+    }
+
+    public static Pizza? GetPizza(int id)
+    {
+        return _pizzas.SingleOrDefault(pizza => pizza.Id == id);
+    }
+
+    public static Pizza CreatePizza(Pizza pizza)
+    {
+        _pizzas.Add(pizza);
+        return pizza;
+    }
+
+    public static Pizza UpdatePizza(Pizza update)
+    {
+        _pizzas = _pizzas.Select(pizza =>
+        {
+            if (pizza.Id == update.Id)
+            {
+                pizza.Name = update.Name;
+            }
+            return pizza;
+        }).ToList();
+        return update;
+    }
+
+    public static void RemovePizza(int id)
+    {
+        _pizzas = _pizzas.FindAll(pizza => pizza.Id != id).ToList();
+    }
+}
